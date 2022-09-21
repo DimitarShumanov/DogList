0b42f3c80771c34e3f45cd04c1c24618adc5308a
User <User@3301-11>
2022-09-21 10:38:39 +0300

    Add project files.

---
+++ DogList/Models/DogList.cs
@@ -0,0 +1,9 @@
+﻿namespace DogList.Models
+{
+    public class DogList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
